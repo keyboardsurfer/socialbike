9a9199994464b9cbfc5fbb1b763f65df4b900a74
keyboardsurfer <keyboardsurfer@c-base.org>
2011-09-13 21:40:41 +0200

    Added Android skeleton

---
+++ Android/src/org/cbase/dev/adkbike/ADKBikeActivity.java
@@ -0,0 +1,15 @@
+package org.cbase.dev.adkbike;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ADKBikeActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
