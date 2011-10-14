c0c21848cbc2448f4695f7f71b2c144ff966c712
keyboardsurfer <keyboardsurfer@c-base.org>
2011-10-14 20:33:20 +0200

    some stuff from Google's adk demokit

---
+++ Android/src/org/cbase/dev/adkbike/SocialBikeActivity.java
@@ -0,0 +1,13 @@
+package org.cbase.dev.adkbike;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SocialBikeActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
